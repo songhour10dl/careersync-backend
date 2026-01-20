0c7361947035777b1da73d8ce59eb77f55ebd03b
hour <Songhour10dl@gmail.com>
2026-01-20 22:27:05 +0700

    thunh

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -18,7 +18,7 @@ app.use(
       "https://careersync-4be.ptascloud.online",
       "https://mentor-4be.ptascloud.online",
       "https://api-4be.ptascloud.online",
-      "https://admin-4be.ptascloud.online/",
+      "https://admin-4be.ptascloud.online",
     ], // Automatically accepts the incoming website address
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
