fb7bae86dbcf7aa6c21337d06ccac0cc440b8a35
hour <Songhour10dl@gmail.com>
2026-01-21 02:27:13 +0700

    changed to node mailer

--- Backend/src/utils/sendEmail.js
+++ Backend/src/utils/sendEmail.js
@@ -1,7 +1,13 @@
 // src/utils/sendEmail.js
+const nodemailer = require("nodemailer");
 
-const sgMail = require("@sendgrid/mail");
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.GMAIL_USER,     // careersyn3@gmail.com
+    pass: process.env.GMAIL_APP_PASS, // Gmail App Password
+  },
+});
 
 const sendEmail = async ({ to, subject, html }) => {
   if (!to) {
@@ -9,14 +15,15 @@ const sendEmail = async ({ to, subject, html }) => {
     throw new Error("Recipient email missing");
   }
 
-  const msg = {
+  const mailOptions = {
+    from: `"CareerSync Security" <${process.env.GMAIL_USER}>`,
     to: to.trim(),
-    from: process.env.EMAIL_FROM,
     subject,
     html,
+    text: html.replace(/<[^>]*>/g, ""), // important: plain text fallback
   };
 
-  return sgMail.send(msg);
+  return transporter.sendMail(mailOptions);
 };
 
 module.exports = sendEmail;
